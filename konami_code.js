2b86fe80af0773c4d782ca9a2727e9f595abfbd4
Everett <everett.bowline@gmail.com>
2018-02-28 02:12:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,6 +5,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0
 
-  document.body.addEventListener function(e) {
+  document.body.addEventListenerfunction(e) {
   const key = parseInt(e.detail || e.which);
 }
